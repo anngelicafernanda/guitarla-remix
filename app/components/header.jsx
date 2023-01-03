18b7703c7b2449a0fbed679b631231cca1cac542
anngelicafernanda <devanngelc@gmail.com>
2023-01-03 20:19:10 -0300

    Creacion del header

---
+++ app/components/header.jsx
@@ -0,0 +1,32 @@
+import { Link, useLocation } from "@remix-run/react";
+import logo from "../../public/img/logo.svg";
+
+const Header = () => {
+  const location = useLocation();
+
+  return (
+    <header className="header">
+      <div className="contenedor barra">
+        <Link to="/">
+          <img className="logo" src={logo} alt="Imagen Logo" />
+        </Link>
+        <nav className="navegacion">
+          <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+            Inicio
+          </Link>
+          <Link to="/nosotros" 
+          className={location.pathname === "/nosotros" ? "active" : "" }
+          >Nosotros</Link>
+          <Link to="/tienda" 
+          className={location.pathname === "/tienda" ? "active" : "" }
+          >Tienda</Link>
+          <Link to="/blog"
+          className={location.pathname === "/blog" ? "active" : "" }
+          >Blog</Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
