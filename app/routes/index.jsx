8bb3bddb49458c1060a6e9e61803139cd00f010f
anngelicafernanda <devanngelc@gmail.com>
2023-01-09 14:18:37 -0300

    Index con datos

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,9 +1,52 @@
-function Index() {
-  return (
-    <div>
-      Desde index.jsx
-    </div>
-  )
+import { useLoaderData } from "@remix-run/react";
+import { getGuitarras } from "~/models/guitarras.server";
+import { getPosts } from "~/models/posts.server";
+import ListadoGuitarras from "~/components/listado-guitarras"
+import stylesGuitarras from "~/styles/guitarras.css"
+import ListadoPosts from "~/components/listado-posts"
+import stylesPosts from "~/styles/blog.css"
+
+export function meta() {}
+
+export function links() {
+ return [
+    {
+      rel: "stylesheet",
+      href: stylesGuitarras
+    },
+    {
+      rel: "stylesheet",
+      href: stylesPosts
+    }
+  ]
 }
 
-export default Index
+export async function loader() {
+  const [guitarras, posts] = await Promise.all([getGuitarras(), getPosts()]);
+
+  return {
+    guitarras: guitarras.data,
+    posts: posts.data
+  };
+}
+
+function Index() {
+  const { guitarras, posts } = useLoaderData();
+ 
+
+  return (
+    <>
+    <main className="contenedor">
+     <ListadoGuitarras
+     guitarras={guitarras}
+     />
+    </main>
+    <section className="contenedor">
+      <ListadoPosts
+      posts={posts}/>
+    </section>
+    </>
+  );
+}
+
+export default Index;
